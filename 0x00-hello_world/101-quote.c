bbf1b1b9dcda8e2e63b48baabf1ff0b22a522b33
John-Shewa <eldi2002@gmail.com>
2022-12-09 00:40:49 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  *
- *  * main - Prints out the last part of a quote in the standard error.
- *
- *   * Return: 1 if success.
- *
- *    */
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
+ */
 
 int main(void)
 
