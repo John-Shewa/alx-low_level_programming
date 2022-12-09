d4dd8be69766784e76a1bb024b442906b2c5f82d
John-Shewa <eldi2002@gmail.com>
2022-12-09 12:51:29 -0800

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ int main(void)
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 		if (n > 0)
-			{printf(n, "is positive");
+			{printf("%d is positive\n", n);
 		}
-		else if (n < 0)
-			{printf(n, "is negative");
+		else if (n == 0)
+			{printf("%d is zero", n);
 		}
 		else
-			{printf(n, "is zero");
+			{printf("%d is negative", n);
 		}
 		return (0);
 }
