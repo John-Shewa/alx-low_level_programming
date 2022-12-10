089ac5033a61afb6260968281f4b6e7631411ee5
John-Shewa <eldi2002@gmail.com>
2022-12-10 07:47:54 -0800

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,12 @@ int main(void)
 
 {
 	int n;
+	char l;
 
-	for (n = 0 ; n <= 16 ; n++)
-	putchar(n);
+	for (n = 0 ; n <= 10 ; n++)
+	putchar((num % 10) + '0';
+	for (l = 'a'; l <= 'f'; l++)
+	putchar(l);
 	putchar('\n');
 	return (0);
 }
