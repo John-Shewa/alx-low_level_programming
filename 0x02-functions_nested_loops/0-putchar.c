0ebce006940a9439015beb983fc040e984adb8be
John-Shewa <eldi2002@gmail.com>
2022-12-13 11:45:02 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
+#include <main.h>
 /**
  * main - putchar
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	{
-	int _putchar(char 0);
+	_putchar('0');
 	putchar('\n');
-	}
 	return (0);
 }
