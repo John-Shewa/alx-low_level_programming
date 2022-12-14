1646cbcb4b77cee30abcfb2020462da6b08ba22d
John-Shewa <eldi2002@gmail.com>
2022-12-14 02:40:30 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - putchar
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+	
+	for (ch = 'a'; ch < 'z'; ch++)
+	{
+	_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
