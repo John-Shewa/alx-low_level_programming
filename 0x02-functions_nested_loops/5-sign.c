65df4e3dc5ffc03dc8f293dd8bcb896089d6534d
John-Shewa <eldi2002@gmail.com>
2022-12-14 11:03:54 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - check main
+ * @n: An input number
+ * Description: checks if c is a letter or not a letter
+ * Return: 1 if it is a letter or 0 if it is 0 or
+ * -1 if it is not a letter
+ */
+int print_sign(int n)
+{
+	int num;
+
+	if (num > 0)
+	{
+		num = 1;
+		_putchar('n');
+	}
+	else if (num == 0)
+	{
+		num = 0;
+		_putchar('0');
+	}
+	else
+	{
+		num = -1;
+		putchar('-');
+	}
+	return (num);
+}
