ff8ab3a0e5baf3c0caa36b816a7858cd6441ecf0
John-Shewa <eldi2002@gmail.com>
2022-12-14 05:15:48 -0800

    alphabet10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int i = 0;
+	int i;
+	char ch;
 
-	while (i < 10)
-{
-	char ch = 'a';
-
-	while (ch <= 'z')
-	_putchar(ch);
-}
-	_putchar('\n');
-	i++;
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
 }
