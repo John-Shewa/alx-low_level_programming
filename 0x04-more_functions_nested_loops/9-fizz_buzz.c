3d4cf57ffe39e5adc922164e19f2d5beec788b91
John-Shewa <eldi2002@gmail.com>
2022-12-16 06:36:05 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
  * main - entry point
  * Return: Always 0
@@ -8,18 +8,18 @@ int main(void)
 {
 	int i = 1;
 
-	for (; i <= 100; i++)
+	for (; i < 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
 		else
 			printf("%d ", i);
 	}
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
