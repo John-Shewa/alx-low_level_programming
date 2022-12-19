937f08cf8b619eb441193bf8ab40c947020b7e64
John-Shewa <eldi2002@gmail.com>
2022-12-19 11:32:23 -0800

    commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  *  _strcpy - a function that copies the string pointed to by src,
  * including the terminating null byte (\0),
@@ -5,7 +6,7 @@
  *
  * @dest: A pointer to destination of string
  * @src: A pointer to source string to copy from
- * 
+ *
  * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
