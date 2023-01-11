6b393963869d2ed5895483ebfe07ffa7863df966
John-Shewa <eldi2002@gmail.com>
2023-01-11 10:23:03 -0800

    commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a program that prints all arguments it recieves
+ * @argc: argument counter
+ * @argv: argument values
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - a program that multiplies two numbers
+ * @argc: argument counter
+ * @argv: argument values
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
+}
