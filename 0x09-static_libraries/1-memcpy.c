96c5bcea7da88b2fdc27e3ba3487b85afbd55a1e
John-Shewa <eldi2002@gmail.com>
2023-01-11 04:28:50 -0800

    commit

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -3,6 +3,7 @@
  * _memcpy - a function that copies memory area
  * @dest: memory area destination to copy
  * @src: memory area source to copy from
+ * @n: number of bytes
  * Return: returns pointer to copy location
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
