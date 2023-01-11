426396fea906db8606ecaee936c568c5cb92736c
John-Shewa <eldi2002@gmail.com>
2023-01-11 03:24:42 -0800

    commit

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,1 +1,11 @@
-int _putchar(char c);
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: on success 1,on error -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
