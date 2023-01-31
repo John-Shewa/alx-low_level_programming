4604e685e34423930bda0afd51acf7ec02249069
John-Shewa <eldi2002@gmail.com>
2023-01-31 05:47:22 -0800

    commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 /**
  * before - print before main
  */
-void before(void) _attribute_((constructor));
+void before(void) __attribute__((constructor));
 void before(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
