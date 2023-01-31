f4483f25db722950f653e53832ee0ef29783ab90
John-Shewa <eldi2002@gmail.com>
2023-01-31 04:28:00 -0800

    commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,9 +4,10 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string
- * @len: length of string
- * @next: points to next node
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
  * Description: singly linked list node structure
  */
 typedef struct list_s
@@ -21,4 +22,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-#endif
+#endif /*_LISTS_H_*/
